96df7e797af8064458224057e9d1c67a7fc15bf9
marisha-shine <ras-07@inbox.ru>
2022-02-22 23:07:39 +0300

    Добавила подключение к mysql

--- api/models/Group.js
+++ api/models/Group.js
@@ -6,7 +6,6 @@
  */
 
 module.exports = {
-  dominant: true,
   attributes: {
     name: {
       type: 'string',
--- config/datastores.js
+++ config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://root:Qa1Ws2Ed3Rf4@localhost:3306/skating',
 
   },
 
